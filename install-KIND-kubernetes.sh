9470d99dfe65a108f02d1b012c63352e436c789c
akshay9811 <akshay.apsdk@gmail.com>
2025-01-22 00:52:49 +0530

    file to install KIND Kubernetes in EC2

---
+++ install-KIND-kubernetes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+########################################################
+#Author: Akshay
+#Date: 1/22/2025
+# This file can be used for the installation of Kubernetes KIND cluster
+#########################################################
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
+chmod +x ./kind
+sudo cp ./kind /usr/local/bin/kind
+
+VERSION="v1.30.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+rm -f kubectl
+rm -rf kind
+
+echo "kind & kubectl installation complete."
